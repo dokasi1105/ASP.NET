d38abd5b9bb931657319791cc590acd3e73107c0
Viet Anh <Viet Anh@DESKTOP-869BICH>
2026-03-19 00:04:21 +0700

    sửa lại giao diện admin,product

--- Thi Web/Program.cs
+++ Thi Web/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
